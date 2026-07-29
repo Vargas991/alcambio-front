8d30f0be3cfb70dd57e0ca3bfbeed6bd419551f7
Vargas991 <eddersonvargas91@gmail.com>
2026-07-28 21:37:47 -0400

    fix footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="py-2 text-gray-600">
       <div className="flex w-full flex-wrap items-center justify-center gap-6 px-2 md:justify-between">
         <p className="font-sans text-sm font-normal leading-normal">
-          © {new Date().getFullYear()} AlCambio. Sistema interno de gestión de divisas.
+          © {new Date().getFullYear()} Sistema interno de gestión de Financiera.
         </p>
 
         <ul className="flex items-center gap-4">
